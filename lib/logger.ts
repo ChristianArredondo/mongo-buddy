b91d1604813668a04e3ecf26560bccaa070fb82e
christianarredondo <christianarredondo@utexas.edu>
2018-07-04 10:36:02 -0500

    set up project with initial MongoDb and Logger classes

---
+++ lib/logger.ts
@@ -0,0 +1,25 @@
+import chalk from 'chalk';
+
+export class Logger {
+
+  public divider(): void {
+    console.log(chalk.grey('==============================================='));
+  }
+
+  public breakLine(): void {
+    console.log('\n');
+  }
+
+  public mongoDetails(mongoUri: string, dbName: string, collections: string) {
+    this.divider();
+    this.breakLine();
+    console.log(chalk.greenBright(`Successfully connected to: ${chalk.magentaBright.bold(mongoUri)}`));
+    console.log(chalk.greenBright(`Database in use: ${chalk.yellow.bold(dbName)}`));
+    console.log(chalk.greenBright(`Available collections: ${chalk.cyanBright.bold(collections)}`));
+    this.breakLine();
+    this.divider();
+  }
+  
+}
+
+export const logger = new Logger();
---
+++ lib/mongodb.ts
@@ -0,0 +1,20 @@
+import { MongoClient, Db } from 'mongodb';
+import chalk from 'chalk';
+import { logger } from './logger';
+
+export class MongoDb {
+  private _db: Db;
+
+  public async connect(mongoUri: string, dbName: string): Promise<void> {
+    const client = await MongoClient.connect(mongoUri, { useNewUrlParser: true });
+    this._db = client.db(dbName);
+    const colls = await this._db.collections();
+    const availableCollections = colls
+      .map(collection => (<any>collection).s.name)
+      .join(', ');
+    logger.mongoDetails(mongoUri, dbName, availableCollections);
+    client.close();
+  }
+}
+
+export const mongodb = new MongoDb();
