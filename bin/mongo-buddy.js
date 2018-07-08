865095c46c09c6a8d7bd4483446fba6488245b03
christianarredondo <christianarredondo@utexas.edu>
2018-07-08 07:31:33 -0500

    add command and workflow to generate new model - no file yet

--- bin/mongo-buddy.js
+++ bin/mongo-buddy.js
@@ -3,6 +3,26 @@
 const mongoBuddy = require('commander');
 const chalk = require('chalk');
 const { mongodb } = require('../lib/mongodb');
+const { logger } = require('../lib/logger');
+
+Array.prototype.toMongoBuddyModel = function() {
+  const model = {}, errors = [];
+  for (let i = 0; i < this.length; i += 2) {
+    let argStr = this[i], [prop, val, ...extras] = this[i+1].split(':');
+    if (prop && !val) {
+      errors.push(`Property "${prop}" has no associated type (i.e. "${prop}:string").`);
+    } else if (!prop && val) {
+      errors.push(`Type "${val}" has no matching property name`);
+    } else if ((extras || []).length) {
+      errors.push(`Property "${prop}" has multiple associated types: ${[val, ...extras].join(':')}. Properties may only have one associated type (i.e. "${prop}:${val}").`)
+    }
+    model[prop] = val;
+  }
+  if (errors.length) {
+    throw new Error(['\n', ...errors].join('\n'));
+  }
+  return model;
+}
 
 mongoBuddy.version('1.0.0');
 
@@ -37,6 +57,31 @@ mongoBuddy
       process.exit(1);
     }
     mongodb.getSingleDoc(uri, db, coll);
+  });
+
+mongoBuddy
+  .command('gen-model')
+  .alias('gm')
+  .description('Generate document model to be used for generating mock data')
+  .option('--n <name>', 'model name (required)')
+  .option('--p <prop>', 'property name and type (can be multiple, i.e. --p name:string --p age:int)')
+  .action((a) => {
+    const props = a.parent.rawArgs.slice(3,);
+    if (!props.length) {
+      console.error(chalk.redBright('You must supply at least one property!'));
+      return;
+    } else if (props.length % 2 !== 0) {
+      console.error(chalk.redBright('You must supply a value for each property!'));
+      return;
+    }
+    try {
+      const model = props.toMongoBuddyModel();
+      console.log(chalk.greenBright(`New model "User" successfully created!`));
+      logger.json(model);
+    } catch (error) {
+      console.log(chalk.redBright(error));
+      process.exit(1);
+    }
   });
 
 mongoBuddy.parse(process.argv);
