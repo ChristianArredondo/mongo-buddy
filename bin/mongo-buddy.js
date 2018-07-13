481f07507ce2fb8c1206f2020ec1ea2942e361ea
christianarredondo <christianarredondo@utexas.edu>
2018-07-13 11:44:35 -0500

    move `getMongoBuddeModel` to util

--- bin/mongo-buddy.js
+++ bin/mongo-buddy.js
@@ -5,32 +5,7 @@ const mongoBuddy = require('commander');
 const chalk = require('chalk');
 const { mongodb } = require('../lib/mongodb');
 const { logger } = require('../lib/logger');
-
-Array.prototype.toMongoBuddyModel = function() {
-  const model = {}, errors = [];
-  const props = this.filter(el => el === '--p' || el.includes(':'));
-  if (!props.length) {
-    errors.push('You must supply at least one property-type pair!');
-  } else if (props.length % 2 !== 0) {
-    errors.push('You must supply a property:type pair for each argument! (i.e. "name:string").');
-  } else {
-    for (let i = 0; i < props.length; i += 2) {
-      let [prop, type, ...extras] = props[i+1].split(':');
-      if (prop && !type) {
-        errors.push(`Property "${prop}" has no associated type (i.e. "${prop}:string").`);
-      } else if (type && !prop) {
-        errors.push(`Type "${type}" has no matching property name (i.e. "foo:${type}").`);
-      } else if ((extras || []).length) {
-        errors.push(`Property "${prop}" has multiple associated types: "${[type, ...extras].join(':')}". Properties may only have one associated type (i.e. "${prop}:${type}").`)
-      }
-      model[prop] = type;
-    }
-  }
-  if (errors.length) {
-    throw new Error(['\n', ...errors].join('\n'));
-  }
-  return model;
-}
+const { createModelJson } = require('../utils/arrToModel.util')
 
 mongoBuddy.version('1.0.0');
 
@@ -68,8 +43,8 @@ mongoBuddy
   });
 
 mongoBuddy
-  .command('gen-model')
-  .alias('gm')
+  .command('model')
+  .alias('m')
   .description('Construct document model used for generating mock data')
   .option('--name <name>', 'model name (required)')
   .option('--p <prop>', 'property name and type (can be multiple, i.e. --p name:string --p age:int)')
@@ -79,10 +54,10 @@ mongoBuddy
     } else if (!name || typeof name !== 'string') {
       return console.error(chalk.redBright('Error:\n \nYou must supply a name!')); // ensure name is inputted
     }
-    const props = parent.rawArgs.slice(3,);
-    props.splice(props.indexOf(name) - 1, 2); // splice name from arguments
+    const args = parent.rawArgs.slice(3,);
+    args.splice(args.indexOf(name) - 1, 2); // splice name and arg-string from arguments array
     try {
-      const model = props.toMongoBuddyModel(); // use custom Array method to convert arguments to model object
+      const model = createModelJson(args); // use custom Array method to convert arguments to model object
       const dataForFile = JSON.stringify(model, null, 2); // stringify with linebreaks for better readability at file
       const fileName = `models/${name}.json`; // name of file to be created
       if (!fs.existsSync('models')) {
@@ -95,6 +70,19 @@ mongoBuddy
       console.log(chalk.redBright(error));
       process.exit(1);
     }
+  });
+
+mongoBuddy
+  .command('data')
+  .alias('d')
+  .description('Generate mock data based on an existing JSON model')
+  .option('--path <path>', 'path to JSON model (required)')
+  .option('--uri [uri]', 'mongo uri', 'mongodb://localhost:27017')
+  .option('--db <db>', 'database name (required)')
+  .option('--coll <coll>', 'collection name (required)')
+  .action(buddy => {
+    // TODO
+    console.log(buddy);
   });
 
 mongoBuddy.parse(process.argv);
---
+++ utils/arrToModel.util.js
@@ -0,0 +1,37 @@
+/**
+ * Converts array of cli command arguments to JSON model file
+ * @param {string []} args
+ * @example
+ * 
+ * INPUT
+ * ['--p', 'name:string', '--p', 'email:email']
+ * 
+ * OUTPUT JSON FILE
+ * { "name": "string", "email": "email" }
+ */
+function createModelJson(args) {
+  const model = {}, errors = [];
+  if (!args.length) {
+    errors.push('You must supply at least one property-type pair!');
+  } else if (args.length % 2 !== 0) {
+    errors.push('You must supply a property:type pair for each argument! (i.e. "name:string").');
+  } else {
+    for (let i = 0; i < args.length; i += 2) {
+      let [prop, type, ...extras] = args[i+1].split(':');
+      if (prop && !type) {
+        errors.push(`Property "${prop}" has no associated type (i.e. "${prop}:string").`);
+      } else if (type && !prop) {
+        errors.push(`Type "${type}" has no matching property name (i.e. "foo:${type}").`);
+      } else if ((extras || []).length) {
+        errors.push(`Property "${prop}" has multiple associated types: "${[type, ...extras].join(':')}". Properties may only have one associated type (i.e. "${prop}:${type}").`)
+      }
+      model[prop] = type;
+    }
+  }
+  if (errors.length) {
+    throw new Error(['\n', ...errors].join('\n'));
+  }
+  return model;
+}
+
+module.exports = { createModelJson }
