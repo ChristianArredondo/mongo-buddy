7409d5c26e24d5104aa766f49ff7fc0391df35ef
christianarredondo <christianarredondo@utexas.edu>
2018-07-13 12:48:05 -0500

    add tests for errors

--- test/test.js
+++ test/test.js
@@ -1,17 +1,26 @@
 const { expect } = require('chai');
 const { getMongoBuddyModel } = require('../utils/arrToModel.util')
 
-describe('MongoBuddy Model Generator', function() {
-  it('should return a valid json that matches property arguments', function() {
-    // 1. ARRANGE
+describe('model-generator', function() {
+  it('should return a valid json matching argument', function() {
     const args = ['--p', 'username:string', '--p', 'email:email']
-
-    // 2. ACT
     const model = getMongoBuddyModel(args);
-
-    // 3. ASSERT
     expect(Object.keys(model).length).to.equal(2);
     expect(model['username']).to.equal('string');
     expect(model['email']).to.equal('email');
-  })
+  });
+
+  it('should throw an error if any props are missing associated type', function() {
+    const args = ['--p', 'username:string', '--p', 'email:'];
+    expect(
+      function() { getMongoBuddyModel(args) }
+    ).to.throw(Error);
+  });
+
+  it('should throw an error if any types are missing associated prop', function() {
+    const args = ['--p', ':string', '--p', 'email:'];
+    expect(
+      function() { getMongoBuddyModel(args) }
+    ).to.throw(Error);
+  });
 });
